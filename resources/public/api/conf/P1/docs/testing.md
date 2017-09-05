c04aaf462f095c116455c3d7e6dcb32c96365f1b
Edgar Jimenez <edgarjimenez68@gmail.com>
2017-09-05 15:48:35 +0100

    OGD-420 Publish API individuals-matching-api

---
+++ resources/public/api/conf/P1/docs/testing.md
@@ -0,0 +1,9 @@
+You can use the [HMRC Developer Sandbox](https://test-developer.service.hmrc.gov.uk/api-documentation/docs/sandbox/introduction) 
+to test the API. The Sandbox is an enhanced testing service that simulates HMRC's production environment.
+
+The Sandbox service for this API supports all the happy path responses and error scenarios described under Resources.
+
+You can set up test data for this API using the following ressources:
+* [Create an individual](https://test-developer.service.hmrc.gov.uk/api-documentation/docs/api/service/api-platform-test-user/1.0#_create-a-test-user-which-is-an-individual_post_accordion).
+* [Create an employer](https://test-developer.service.hmrc.gov.uk/api-documentation/docs/api/service/api-platform-test-user/1.0#_create-a-test-user-which-is-an-organisation_post_accordion).
+* [Create an employment](https://test-developer.service.hmrc.gov.uk/api-documentation/docs/api/service/individual-income-des-stub/1.0#_create-an-employment_post_accordion).
---
+++ test/unit/uk/gov/hmrc/individualsmatchingapi/play/RequestHeaderUtilsSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.individualsmatchingapi.play
+
+import org.scalatest.Matchers
+import play.api.test.FakeRequest
+import uk.gov.hmrc.individualsmatchingapi.play.RequestHeaderUtils._
+import play.api.test.Helpers.{ACCEPT, GET}
+import uk.gov.hmrc.play.test.UnitSpec
+
+class RequestHeaderUtilsSpec extends UnitSpec with Matchers {
+
+  "getVersionedUri" should {
+    "return the versioned request when the Accept header is set" in {
+      val fooRequest = FakeRequest(GET, "/foo")
+
+      getVersionedRequest(fooRequest.withHeaders(ACCEPT -> "application/vnd.hmrc.2.0+json")).uri shouldBe "/v2.0/foo"
+      getVersionedRequest(fooRequest.withHeaders(ACCEPT -> "application/vnd.hmrc.2.0+json")).path shouldBe "/v2.0/foo"
+    }
+
+    "return the versioned request for the root endpoint when the Accept header is set" in {
+      val fooRequest = FakeRequest(GET, "/")
+
+      getVersionedRequest(fooRequest.withHeaders(ACCEPT -> "application/vnd.hmrc.2.0+json")).uri shouldBe "/v2.0"
+      getVersionedRequest(fooRequest.withHeaders(ACCEPT -> "application/vnd.hmrc.2.0+json")).path shouldBe "/v2.0"
+    }
+
+    "Default to 1 when the Accept header is not set" in {
+      val fooRequest = FakeRequest(GET, "/foo")
+
+      getVersionedRequest(fooRequest).uri shouldBe "/v1/foo"
+      getVersionedRequest(fooRequest).path shouldBe "/v1/foo"
+    }
+  }
+
+  "extractUriContext" should {
+    "extract uri contexts" in {
+      extractUriContext(FakeRequest(GET, "/")) shouldBe "/"
+      extractUriContext(FakeRequest(GET, "/foo")) shouldBe "/foo"
+      extractUriContext(FakeRequest(GET, "/foo/bar")) shouldBe "/foo"
+    }
+  }
+}
