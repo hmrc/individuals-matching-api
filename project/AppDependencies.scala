af8761c2325a3ae9546404263815429af20d7d5d
Tom Rafferty <86563641+TomRafferty@users.noreply.github.com>
2023-02-16 16:45:02 +0000

    [DLS-6065] unified mongo play versions (PR bot suggestion)
    [DLS-6065] spacing fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,6 +10,7 @@ object AppDependencies {
   val akkaHttpVersion = "10.2.6"
 
   val hmrcMongo = "uk.gov.hmrc.mongo"
+  val hmrcMongoVersion = "0.70.0"
 
   val overrides = Seq(
     akka %% "akka-stream" % akkaVersion,
@@ -29,7 +30,7 @@ object AppDependencies {
     hmrc                %% "play-hmrc-api"              % "6.4.0-play-28",
     hmrc                %% "json-encryption"            % "4.10.0-play-28",
     "com.typesafe.play" %% "play-json-joda"             % "2.9.2",
-    hmrcMongo           %% "hmrc-mongo-play-28"         % "0.70.0"
+    hmrcMongo           %% "hmrc-mongo-play-28"         % hmrcMongoVersion
   )
 
   def test(scope: String = "test,it") = Seq(
@@ -41,7 +42,7 @@ object AppDependencies {
     "org.pegdown"            % "pegdown"                   % "1.6.0"             % scope,
     "com.github.tomakehurst" % "wiremock-jre8"             % "2.27.2"            % scope,
     hmrc                     %% "service-integration-test" % "1.1.0-play-28"     % scope,
-    hmrcMongo                %% "hmrc-mongo-test-play-28"  % "0.53.0"            % scope
+    hmrcMongo                %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion    % scope
   )
 
 
