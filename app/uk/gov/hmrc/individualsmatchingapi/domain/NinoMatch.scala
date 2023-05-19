f8420f8de45b567522f7a21d661dc4dd47b3da81
johno1985 <1640705+johno1985@users.noreply.github.com>
2023-05-19 12:48:44 +0100

    DLS-8076: Use Json.format macro

--- app/uk/gov/hmrc/individualsmatchingapi/domain/NinoMatch.scala
+++ app/uk/gov/hmrc/individualsmatchingapi/domain/NinoMatch.scala
@@ -16,27 +16,14 @@
 
 package uk.gov.hmrc.individualsmatchingapi.domain
 
-import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
-
-import java.util.UUID
-import play.api.libs.json.{Format, JsPath}
+import play.api.libs.json.{Json, OFormat}
 import uk.gov.hmrc.domain.Nino
-import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats.Implicits._
+
 import java.time.{LocalDateTime, ZoneOffset}
+import java.util.UUID
 
 case class NinoMatch(nino: Nino, id: UUID, createdAt: LocalDateTime = LocalDateTime.now(ZoneOffset.UTC))
 
 object NinoMatch {
-  implicit val format: Format[NinoMatch] = Format(
-    (
-      (JsPath \ "nino").read[Nino] and
-        (JsPath \ "id").read[UUID] and
-        (JsPath \ "createdAt").read[LocalDateTime]
-    )(NinoMatch.apply _),
-    (
-      (JsPath \ "nino").write[Nino] and
-        (JsPath \ "id").write[UUID] and
-        (JsPath \ "createdAt").write[LocalDateTime]
-    )(unlift(NinoMatch.unapply))
-  )
+  implicit val format: OFormat[NinoMatch] = Json.format[NinoMatch]
 }
