5b21557dc4fcde7f443dfee4e6a80100bcd5a5f4
Karolina Zaborska <karolina.zaborska@digital.hmrc.gov.uk>
2024-10-07 18:22:08 +0100

    DLS-10940: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val hmrcMongo = s"$hmrc.mongo"
   val playVersion = "play-30"
   val hmrcMongoVersion = "1.7.0"
-  val hmrcBootstrapVersion = "8.4.0"
+  val hmrcBootstrapVersion = "9.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     hmrc      %% s"bootstrap-backend-$playVersion" % hmrcBootstrapVersion,
@@ -17,8 +17,8 @@ object AppDependencies {
 
   def test(scope: String = "test, it"): Seq[ModuleID] = Seq(
     hmrc                   %% s"bootstrap-test-$playVersion" % hmrcBootstrapVersion % scope,
-    "org.mockito"          %% "mockito-scala"                % "1.17.30"            % scope,
-    "org.scalatestplus"    %% "scalacheck-1-17"              % "3.2.17.0"           % scope,
+    "org.mockito"          %% "mockito-scala"                % "1.17.37"            % scope,
+    "org.scalatestplus"    %% "scalacheck-1-17"              % "3.2.18.0"           % scope,
     "org.scalaj"           %% "scalaj-http"                  % "2.4.2"              % scope,
   )
 }
